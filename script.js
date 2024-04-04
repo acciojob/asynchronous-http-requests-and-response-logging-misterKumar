ec508721027f0f51c25f8876eeecd59f4e7ab94c
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-04-04 11:06:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+async function fun1(){
+	try {
+	const res=await fetch("https://jsonplaceholder.typicode.com/posts/1")
+		const data=await res.json();
+		console.log(data);
+	} catch (error) {
+		console.log(error);
+	}
+}
+async function fun2(){
+	try {
+	const res=await fetch("https://jsonplaceholder.typicode.com/posts/2")
+		const data=await res.json();
+		console.log(data);
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+async function fun3(){
+	try {
+	const res=await fetch("https://jsonplaceholder.typicode.com/posts/3")
+		const data=await res.json();
+		console.log(data);
+	} catch (error) {
+		console.log(error);
+	}
+}
+fun3();
+fun2();
+fun1();
